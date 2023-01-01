1578cbdd531cc78bafb757aa6b1db57e198c4d6f
Jordie <jordiehendl@gmail.com>
2023-01-01 03:07:29 -0600

    Add assertions for debugging

--- luna/plugin/scheduler.cpp
+++ luna/plugin/scheduler.cpp
@@ -1,24 +1,28 @@
 #include "luna/plugin/scheduler.hpp"
 #include <filesystem>
 #include <iostream>
+#include <cassert>
 namespace luna {
 namespace plugin {
 
 
 Scheduler::Scheduler(std::string_view node_path) {
   if(node_path.empty() || !std::filesystem::exists(node_path)) return;
+  std::cout << "a" << std::endl;
   const auto fs_path = std::filesystem::path(node_path.begin()); 
   for(const auto& dir : std::filesystem::recursive_directory_iterator(fs_path)) {
     auto path = std::string(dir.path().string());
     auto base_filename = std::string(path.substr(path.find_last_of("/\\") + 1));
     const auto p = std::string::size_type(base_filename.find_last_of('.'));
     auto file_without_extension = base_filename.substr(0, p);
-
     auto& loaded = this->m_nodes[file_without_extension];
     loaded.loader.load(path.c_str());
     loaded.initalize = loaded.loader.symbol("initialize");
     loaded.shutdown = loaded.loader.symbol("shutdown");
     loaded.update = loaded.loader.symbol("update");
+    assert(loaded.initalize);
+    assert(loaded.update);
+    assert(loaded.shutdown);
     loaded.initalize();
   }
 }
